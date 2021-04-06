102f3c70b0aba40a5e0fda985e25457cd1d93205
amandacorris <79169785+amandacorris@users.noreply.github.com>
2021-04-06 11:19:00 -0500

    Update index.js
    Took away & operator. 
    Changed to regular for loop.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function displayResults(responseJson, maxResults) {
     $('.js-error-message').empty();
     $('.results-list').empty();
     
-for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+for (let i = 0; i < responseJson.data.length; i++) {
         $('.results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
         <p>${responseJson.data[i].description}</p>
         </li>`);
